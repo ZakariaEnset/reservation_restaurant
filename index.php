b136da96161edb31479afb6e7b9997943a9779a7
ZakariaBouskif <z.bouskif@gmail.com>
2026-01-24 20:16:53 +0100

    remove unnessery comment code

--- index.php
+++ index.php
@@ -1,9 +1,7 @@
 <?php
 
 require_once('src/controllers/homepage.php');
-// ini_set('display_errors', 1);
-// ini_set('display_startup_errors', 1);
-// error_reporting(E_ALL);
+
 
 use Application\Controllers\Homepage\Homepage;
 
